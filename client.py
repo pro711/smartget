0b5161e9947d83f29b787740dfdb107f634bd063
lighteningman@gmail.com <lighteningman@gmail.com@947a477d-5cd2-6ce5-780f-c35a845aa365>
2011-02-07 06:20:00 +0000

    request a file from client and the server will download it for client

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 
 # receive file from serve and save as destination
 dest = 'destination'
+url = 'http://www.xdowns.com/soft/xdowns2009.asp?softid=2767&downid=49&id=2767'
 
 import socket
 import os
@@ -10,10 +11,14 @@ s = socket.socket()
 host = socket.gethostname()
 port = 1234
 s.connect(('127.0.0.1',1234))
+s.send(url)
+
 fp = open(dest,'ab')
-fp.write(s.recv(1024))
 file = s.recv(1024)
+
 while file:
+    print 'received 1kB'
     fp.write(file)
     file = s.recv(1024)
 fp.close()
+
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ source = 'source'
 
 import socket
 import os
+from download import downloadurl
 
 s = socket.socket()
 host = socket.gethostname()
@@ -12,11 +13,13 @@ port = 1234
 s.bind((host,port))
 s.listen(5)
 
-fp = open(source,'rb')
-file = fp.read()
-fp.close()
+# fp = open(source,'rb')
+# file = fp.read()
+# fp.close()
 while True:
     c,addr=s.accept()
     print 'Got connection from',addr
-    c.send(file)
+    url = c.recv(1024)
+    print 'Got url'+url
+    c.send(downloadurl(url))
     c.close()
