c4d9a9bfb4e676be5093aca267db4847e89ff2a7
lighteningman@gmail.com <lighteningman@gmail.com@947a477d-5cd2-6ce5-780f-c35a845aa365>
2011-02-08 10:08:19 +0000

    download speed display

--- client.py
+++ client.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python
 
 # receive file from serve and save as destination
-dest = 'destination'
-url = 'http://www.xdowns.com/soft/xdowns2009.asp?softid=2767&downid=49&id=2767'
+
 MAXBUFFER = 1048576
 
 import socket
 import os
+import time
 
-
-s = socket.socket()
-host = socket.gethostname()
-port = 1234
-s.connect(('127.0.0.1',1234))
-
-request = url + '\n' + str(0) + '\n' + str(0)
-
-s.send(request)
-
-fp = open(dest,'ab')
-file = s.recv(1024)
-buffer = ''
-while file:
-    print 'received 16kB'
-    buffer += file
-    if buffer >= MAXBUFFER:
-        fp.write(file)
-        buffer = ''
+def requestdownload(url,dest):
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 1234
+    s.connect(('127.0.0.1',1234))
+    
+    request = url + '\n' + str(0) + '\n' + str(0)
+    
+    s.send(request)
+    
+    fp = open(dest,'ab')
+    t = tstart = time.time()
     file = s.recv(16384)
-fp.close()
+    buffer = ''
+    wrote = complete = 0
+    speed = 0
+    while file:
+        buffer += file
+        print '%dkB complete. speed = %dkB/s'%(complete,speed)
+        complete += 16
+        if len(buffer) >= MAXBUFFER:
+            fp.write(buffer)
+            buffer = ''
+            speed = (complete - wrote)/(time.time() - tstart)
+            tstart = time.time()
+            wrote = complete
+        file = s.recv(16384)
+    fp.write(buffer)
+    fp.close()
 
+if __name__ == '__main__':
+    requestdownload('http://mirrors.163.com/debian-cd/6.0.0/i386/iso-cd/debian-6.0.0-i386-businesscard.iso','destination')
