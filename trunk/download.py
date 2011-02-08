b9cb677c91dbe255b33fb6263b16626dc82dae61
lighteningman@gmail.com <lighteningman@gmail.com@947a477d-5cd2-6ce5-780f-c35a845aa365>
2011-02-08 09:17:28 +0000

    Support download part of a file from the daemon now.

--- trunk/download.py
+++ trunk/download.py
@@ -2,19 +2,52 @@
 
 import os, urllib
 
-def downloadurl(url):
+class MyURLopener(urllib.FancyURLopener):
+    """My URL opener, extends FancyURLOpener, to define my own
+    user-agents.
+    Example:
+    newopener = MyURLopener()
+    newopener.open(url,start,end)
+    newopener.sock.read(length)
+    newopener.sock.close()
     """
-    Open a URL file and store it on your disk.
+    
+        
+    def openurl(self,url,start='0',end='0'):
+        """download a url
+        
+        Arguments:
+        - `url`:
+        - `start`:
+        - `end`:
+        """
+        self._start = start
+        self._end = end
+        self.addheader('Range','bytes=%s-' %(self._start) + (self._end!='0' and str(self._end) or ''))
+        self.sock = self.open(url)
+
+        
+def GetUrlFileSize(url, proxies={}):
+    """Get the size of url.
     Arguments:
-    - `url`: a string of the url to download
+    - `url`:
+    - `proxies = {}`:
     """
-    sock = urllib.urlopen(url)
-    data = sock.read()
-    sock.close()
-    return data
+    urlHandler = urllib.urlopen( url, proxies=proxies )
+    headers = urlHandler.info().headers
+    length = 0
+    for header in headers:
+        if header.find('Length') != -1:
+            length = header.split(':')[-1].strip()
+            length = int(length)
+            return length
+
+
 
 if __name__ == '__main__':
-    file = downloadurl('http://www.xdowns.com/soft/xdowns2009.asp?softid=2767&downid=49&id=2767')
+    opener = MyURLopener()
+    opener.openurl('http://www.xdowns.com/soft/xdowns2009.asp?softid=2767&downid=49&id=2767')
+    file = opener.sock.read()
     destination = open('destination','wb')
     destination.write(file)
     destination.close()
