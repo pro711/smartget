bb25d93dc2d57331944c6bac1f4becc2074b8b91
lighteningman@gmail.com <lighteningman@gmail.com@947a477d-5cd2-6ce5-780f-c35a845aa365>
2011-02-10 08:20:49 +0000

    error handling and timeout handling

--- client.py
+++ client.py
@@ -12,32 +12,40 @@ def requestdownload(url,dest):
     s = socket.socket()
     host = socket.gethostname()
     port = 1234
-    s.connect(('127.0.0.1',1234))
-    
-    request = url + '\n' + str(0) + '\n' + str(0)
-    
-    s.send(request)
-    
-    fp = open(dest,'ab')
-    t = tstart = time.time()
-    file = s.recv(16384)
-    buffer = ''
-    wrote = complete = 0
-    speed = 0
-    while file:
-        buffer += file
-        print '%dkB complete. speed = %dkB/s'%(complete,speed)
-        complete += 16
-        if len(buffer) >= MAXBUFFER:
-            fp.write(buffer)
-            buffer = ''
-            speed = (complete - wrote)/(time.time() - tstart)
-            tstart = time.time()
-            wrote = complete
+    try:
+        s.connect(('127.0.0.1',1234))
+        
+        request = url + '\n' + str(0) + '\n' + str(0)
+        
+        s.send(request)
+        try:
+            fp = open(dest,'ab')
+        except IOError:
+            print 'Open file Error.'
+            exit()
+        t = tstart = time.time()
         file = s.recv(16384)
-    fp.write(buffer)
-    fp.close()
+        buffer = ''
+        wrote = complete = 0
+        speed = 0
+        while file:
+            buffer += file
+            print '%dkB complete. speed = %dkB/s'%(complete,speed)
+            complete += len(file)/1024
+            if len(buffer) >= MAXBUFFER:
+                fp.write(buffer)
+                buffer = ''
+                speed = (complete - wrote)/(time.time() - tstart)
+                tstart = time.time()
+                wrote = complete
+            file = s.recv(16384)
+        fp.write(buffer)
+        fp.close()
+    except socket.timeout:
+        print 'time out'
+        exit()
 
 if __name__ == '__main__':
+    socket.setdefaulttimeout(5)
     url = sys.argv[1]
     requestdownload(url,sys.argv[1].split('/')[-1])
