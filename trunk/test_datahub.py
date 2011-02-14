924f7d93ca5e616badfd46b4e9ecfa3f935eddb1
pro711 <pro711@947a477d-5cd2-6ce5-780f-c35a845aa365>
2011-02-14 08:55:14 +0000

    Implement node add/remove on data hub.

---
+++ trunk/test_datahub.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       test_datahub.py
+#       
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import os
+import sys
+import logging
+import socket
+
+
+HOST = '127.0.0.1'    # The remote host
+PORT = 20110          # The same port as used by the server
+
+
+def main():
+    for i in range(2):
+        # register
+        for port in range(10001,10010):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            s.send('`register %d' % port)
+            data = s.recv(1024)
+            s.close()
+            print 'Received', repr(data)
+        # deregister
+        for port in range(10001,10010):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            s.send('`deregister %d' % port)
+            data = s.recv(1024)
+            s.close()
+            print 'Received', repr(data)
+
+if __name__ == '__main__':
+	main()
+
