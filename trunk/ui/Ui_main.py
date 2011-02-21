1e60fabad4a7e38cb80d8218d686dc336d0e0db3
lighteningman@gmail.com <lighteningman@gmail.com@947a477d-5cd2-6ce5-780f-c35a845aa365>
2011-02-21 14:36:17 +0000

    multiple downloads and UI improved

--- trunk/ui/Ui_main.py
+++ trunk/ui/Ui_main.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'main.ui'
 #
-# Created: Mon Feb 21 19:18:36 2011
+# Created: Mon Feb 21 22:04:09 2011
 #      by: PyQt4 UI code generator 4.7.3
 #
 # WARNING! All changes made in this file will be lost!
